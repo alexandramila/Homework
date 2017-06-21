b35bf32649501a7e9a9fc6d2787c8ff9a97caf35
Ekaterina <ekaterinashevyako@gmail.com>
2017-06-21 15:09:09 +0300

    Update DEV-1

--- OutputOfNumbers.java
+++ OutputOfNumbers.java
@@ -1,11 +1,11 @@
 class OutputOfNumbers {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 100; i++) {
-            if (i % 3 == 0) {
-                System.out.println("3*" + i/3);
-            } else {
-                System.out.println(i);
-            }    
-        }
-    }
+  public static void main(String[] args) {
+    for (int i = 0; i <= 100; i++) {
+      if (i % 3 == 0) {
+        System.out.println(" 3* " + i / 3);
+      } else {
+        System.out.println(i);
+      }   
+    }      
+  }     
 }
