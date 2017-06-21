112d9141c10b4a1aa70474c4d203497c50ea5d95
Ekaterina <ekaterinashevyako@gmail.com>
2017-06-22 00:46:26 +0300

    Tutti-Frutti

--- OutputOfNumbers.java
+++ OutputOfNumbers.java
@@ -1,10 +1,14 @@
 class OutputOfNumbers {
   public static void main(String[] args) {
     for (int i = 0; i <= 100; i++) {
-      if (i % 3 == 0) {
-        System.out.println(" 3* " + i / 3);
+      if (i % 5 == 0 && i % 3 == 0) {
+        System.out.println("Tutti-Frutti");
+      } else if (i % 5 == 0 && i % 3 != 0) {
+        System.out.println("Frutti");
+      } else if (i % 3 == 0 && i % 5 != 0) {
+        System.out.println("Tutti");
       } else {
-        System.out.println(i);
+        System.out.println(i);     
       }   
     }      
   }     
