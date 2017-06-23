f16f10a86e45a496d831ca9b2c675663d1fb6db6
Ekaterina <ekaterinashevyako@gmail.com>
2017-06-23 23:37:46 +0300

    Enter number

---
+++ Fibonachi.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Fibonachi {
+  public static void main (String[] args) {
+    Scanner in = new Scanner (System.in);
+	System.out.println ("Enter some number ");
+	int input = in.nextInt ();
+	System.out.println (input);
+	}
+}
