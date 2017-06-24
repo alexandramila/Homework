cba665cf3dfa3ee4b77586958686bd71fc0ec035
Ekaterina <ekaterinashevyako@gmail.com>
2017-06-24 11:22:19 +0300

    Проверка на неотрицательное число

--- Fibonachi.java
+++ Fibonachi.java
@@ -2,9 +2,18 @@ import java.util.Scanner;
 
 public class Fibonachi {
   public static void main (String[] args) {
-    Scanner in = new Scanner (System.in);
-	System.out.println ("Enter some number ");
-	int input = in.nextInt ();
-	System.out.println (input);
+    int num = 0;
+	Scanner in = new Scanner(System.in);
+	System.out.println("Enter some number ");
+	String input = in.nextLine();	
+	try {
+        num = Integer.parseInt(input);
+        System.out.println(num);
+    } catch (NumberFormatException e) {
+        System.err.println("Invalid number format!");		
 	}
+	if (num <= 0) {
+		System.out.println("The negative number is entered");
+	}
+  }
 }
