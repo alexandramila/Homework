f410c3fef3f5f02c6cd029a2033c1f36f6acdc97
Ekaterina <ekaterinashevyako@gmail.com>
2017-06-25 14:32:24 +0300

    parts

--- Новая папка/SequenceCheck.java
+++ Новая папка/SequenceCheck.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class SequenceCheck {
   public static void main(String[] args) {
 	Scanner in = new Scanner(System.in);
-	System.out.println("Enter some number:");
+	System.out.println("Enter some sequence:");
 	String input = in.nextLine();
-	String[] sequence = input.split(" ");
-	System.out.println(sequence);	
+	String parts[] = input.split(" "); 
+	for (int i = 0; i < parts.length; i++) {
+	    System.out.println(parts[i]);
+	}	
   }
 }
