5add2b4be3abc41b346dbf5698482acfabc2f4f5
Ekaterina <ekaterinashevyako@gmail.com>
2017-06-25 15:11:25 +0300

    Parts

---
+++ SequenceCheck.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SequenceCheck {
+  public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.println("Enter some number:");
+	String input = in.nextLine();
+	String[] parts = input.split(" ");
+	for (int i = 0; i < parts.length; i++) {
+		System.out.println(parts[i]);
+    }		
+  }
+}
